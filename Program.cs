5d1f6e9b599b92cc69e4dba7b6421c79ef754820
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-12-16 16:47:25 -0300

    criado solução

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int soma = 0;
+            int cont = 0;
+            double media;
+            idade =int.Parse(Console.ReadLine());
+            
+            while (idade >0){
+                soma = soma + idade;
+                cont ++;
+                idade =int.Parse(Console.ReadLine());
+            }
+            media = (double)soma / cont;
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
